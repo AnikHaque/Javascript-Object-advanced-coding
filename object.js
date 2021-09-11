637052c2063b83c842e65daaa8380bba5d072410
EkramulHaqueEngineer <ekramulhaqueengineer@gmail.com>
2021-09-11 17:39:49 +0600

    added

---
+++ object.js
@@ -0,0 +1,26 @@
+// accessing the object 
+    const student = {
+    name:'Ekramul',
+    age:25,
+    graduation:'CSE'
+    }
+    console.log(student)
+
+// accessing the age proprty
+    const studentNew = {
+    name:'Ekramul',
+    age:25,
+    graduation:'CSE'
+    }
+    console.log(student.age)
+
+// accessing an array from an object 
+const teacher = {
+    name:['Malek','Salek','Anik','Ekram'],
+    age:25,
+    graduation:'CSE'
+}
+console.log(teacher.name);
+    
+     
+    
